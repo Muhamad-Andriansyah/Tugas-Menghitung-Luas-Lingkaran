cff8848c2f3baa4d56bd48749e90263b0a499bcd
Muhamad-Andriansyah <45659337+Muhamad-Andriansyah@users.noreply.github.com>
2019-01-03 11:20:44 +0700

    Add files via upload

---
+++ Tugas Menghitung Luas Lingkaran.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "MENCARI LUAS LINGKARAN - MUHAMAD ANDRIANSYAH (311810297) TI18 B2" << endl;
+
+        double r;
+        double phi=3.14;
+        cout<< "Masukan Jari-Jari = ";
+        cin>> r;
+        cout<< "Luas Lingkaran Nya Adalah = "<<(phi)*r*r;
+    return 0;
+}
